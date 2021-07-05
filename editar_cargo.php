3bdb0646ca8a25707edfc611c7c5a1d1811950f3
Pedro Sobucki <62084173+pedrosobucki@users.noreply.github.com>
2021-07-04 22:00:20 -0300

    t

---
+++ editar_cargo.php
@@ -0,0 +1,38 @@
+<?php
+	(isset($_GET['id_cargo'])) ? $id_cargo=$_GET['id_cargo'] : false;
+
+	$query = 'SELECT id_cargo, cargo_nome FROM contato WHERE id_cargo = ?';
+	$stmt = mysqli_stmt_init($link);
+		
+	try{
+		mysqli_stmt_prepare($stmt, $query);
+		mysqli_stmt_bind_param($stmt, "s", $id_cargo);
+		if(mysqli_stmt_execute($stmt)){
+			echo mysqli_stmt_affected_rows($stmt)." row(s) affected!<br>";
+			//die;
+			$estado='ok';
+		}else{
+			$estado='falha';
+		}
+	}catch(Exception $e){
+		echo "An exception occurred, unable to update item!";
+		die;
+	}
+
+	$res=mysqli_stmt_get_result($stmt);
+	while ($linha = mysqli_fetch_array($res)) {
+
+		$id_cargo = $linha['id_cargo'];
+		$cargo_nome = $linha['cargo_nome'];
+
+	}
+?>
+
+<form method="POST" action="acao_usuario.php">
+	<input type="hidden" name="acao" value="editar_cargo">
+	<input type="hidden" name="id_cargo" value="<?php echo $id_cargo?>">
+
+	Cargo: <input type="text" name="cargo" value="<?php echo $cargo_nome?>"><br>
+
+	<input type="submit" name="botao" value="Enviar">
+</form>
